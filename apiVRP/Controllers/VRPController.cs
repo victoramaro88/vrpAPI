223fd2f6e318d46c3c049b35eae12b7beae61790
Victor Amaro <amaro.victor@hotmail.com>
2021-08-11 19:43:29 -0300

    Publicação em Desenvolvimento.

--- apiVRP/Controllers/VRPController.cs
+++ apiVRP/Controllers/VRPController.cs
@@ -124,7 +124,8 @@ namespace apiVRP.Controllers
                         objVRP.temperatura = temperatura;
                         objVRP.pressaoMont = pressaoMont;
                         objVRP.pressaoJus = pressaoJus;
-                        objVRP.vazao = Math.Round((vazao > 0 ? (((retInfo.fatorMultVaz*60) / vazao) * 60)/1000 : 0), 2);//-> m³/hora
+                        // objVRP.vazao = Math.Round((vazao > 0 ? (((retInfo.fatorMultVaz*60) / vazao) * 60)/1000 : 0), 2);//-> m³/hora
+                        objVRP.vazao = Math.Round((vazao > 0 ? ((retInfo.fatorMultVaz) / vazao) : 0), 2);//-> m³/hora
                         objVRP.tensaoBat = tensaoBat;
                         objVRP.idVRP = idVRP;
                         resp = _appVRPRepo.InsereHistVRP(objVRP);
